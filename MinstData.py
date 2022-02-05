1a26b4f7a9c343c6915e760919f93f7d75f7f56a
Frognar <sebastian.przyszlak@gmail.com>
2022-02-05 23:24:08 +0100

    Saving training checkpoints, move data to device in dataloader

--- MinstData.py
+++ MinstData.py
@@ -4,8 +4,9 @@ from torchvision import datasets
 
 
 class MinstDataset():
-    def __init__(self, data_path, batch_size, num_workers):
+    def __init__(self, data_path, device, batch_size, num_workers):
         self.data_path = data_path
+        self.device = device
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.transform = Compose([ToTensor(), Normalize((0.5), (0.5))])
@@ -31,9 +32,27 @@ class MinstDataset():
         )
 
     def get_dataloader(self, dataset):
-        return DataLoader(
-            dataset,
-            self.batch_size,
-            shuffle=True,
-            num_workers=self.num_workers
+        return WrappedDataLoader(
+            DataLoader(
+                dataset,
+                self.batch_size,
+                shuffle=True,
+                num_workers=self.num_workers
+            ),
+            self.device
         )
+
+
+class WrappedDataLoader:
+    def __init__(self, dataloader, device):
+        self.dataloader = dataloader
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataloader)
+
+    def __iter__(self):
+        batches = iter(self.dataloader)
+        for x, y in batches:
+            yield x.to(self.device), y.to(self.device)
+    
--- minstDemo.py
+++ minstDemo.py
@@ -10,27 +10,81 @@ from MinstModel import MinstCNN
 from MinstData import MinstDataset
 
 
-def train(model, num_epochs, dataloader, device, output_dir):
+def parse_argument():
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        '--checkpoint',
+        type=str,
+        default=None,
+        help='Path to checkpoint'
+    )
+    ap.add_argument(
+        '--model',
+        type=str,
+        default=None,
+        help='path to saved model'
+    )
+    ap.add_argument(
+        '--checkpoint_output',
+        type=str,
+        default='./checkpoints',
+        help='checkpoint output directory'
+    )
+    ap.add_argument(
+        '-o', '--output',
+        type=str,
+        default='./',
+        help='output directory'
+    )
+    return vars(ap.parse_args())
+
+def create_dir_if_not_exists(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+def load_checkpoint(checkpoint_path, model, optimizer):
+    if checkpoint_path is None or not os.path.exists(checkpoint_path):
+        raise ValueError(f'Checkpoint path {checkpoint_path} does not exist')
+
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    last_epoch = checkpoint['last_epoch']
+    loss_history = checkpoint['loss_history']
+    return last_epoch, loss_history
+
+def train(model, loss_func, optimizer, epochs,
+        dataloader, checkpoint_dir, last_epoch, loss_history):
     model.train()
-    loss_func = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    loss_history = {}
-    for epoch in range(num_epochs):
-        losses = 0.0
-        for (images, labels) in dataloader:
-            images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()
-            output = model(images)
-            loss = loss_func(output, labels)
-            loss.backward()
-            optimizer.step()
-            losses += loss.item()
-        print(f'[{epoch + 1}/{num_epochs}] loss: {losses / len(dataloader)}')
-        loss_history[epoch] = losses
-        now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-        model_path = f'{output_dir}/minstModel_l{losses}_{now}.pth'
-        torch.save(model.state_dict(), model_path)
-    return loss_history
+    end_epoch = last_epoch + epochs
+    for epoch in range(last_epoch + 1, end_epoch + 1):
+        loss = process_epoch(model, dataloader, loss_func, optimizer)
+        loss_history[epoch] = loss
+        save_checkpoint(checkpoint_dir, model, optimizer, epoch, loss_history)
+        print(f'[{epoch}/{end_epoch}] loss: {loss:.4f}')
+
+def process_epoch(model, dataloader, loss_func, optimizer):
+    epoch_loss = 0.0
+    for images, labels in dataloader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = loss_func(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+    return epoch_loss
+
+def save_checkpoint(checkpoint_dir, model, optimizer, last_epoch, loss_history):
+    torch.save({
+        'last_epoch': last_epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss_history': loss_history
+    }, name_checkpoint(checkpoint_dir, last_epoch))
+
+def name_checkpoint(output_dir, last_epoch):
+    now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    return f'{output_dir}/checkpoint_e{last_epoch}_{now}.pth'
 
 def plot_training_loss(loss_history):
     epochs, losses = loss_history.keys(), loss_history.values()
@@ -41,13 +95,15 @@ def plot_training_loss(loss_history):
     plt.legend()
     plt.show()
 
-def test(model, dataloader, device):
+def save_model(model, model_path):
+    torch.save(model.state_dict(), model_path)
+
+def test(model, dataloader):
     model.eval()
     with torch.no_grad():
         correct = 0
         total = 0
         for images, labels in dataloader:
-            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
@@ -55,46 +111,33 @@ def test(model, dataloader, device):
         accuracy = 100 * correct / total
         print(f'Test Accuracy of the model on the test images: {accuracy:.1f}')
 
-def parse_argument():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('--model', help='path to saved model')
-    ap.add_argument('-o', '--output', help='output directory', default='.')
-    return vars(ap.parse_args())
-
-def create_dir_if_not_exists(dir_path):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-def get_model(model_path=None):
-    model = MinstCNN()
-    if model_path is not None and os.path.exists(model_path):
-        model.load_state_dict(torch.load(model_path))
-        print(f'Loaded model from {model_path}')
-    return model
-
 if __name__ == '__main__':
     args = parse_argument()
+    checkpoint = args['checkpoint']
+    model_path = args['model']
+    checkpoint_output_dir = args['checkpoint_output']
     output_dir = args['output']
-
     create_dir_if_not_exists(output_dir)
-    net = get_model(args['model'])
+    create_dir_if_not_exists(checkpoint_output_dir)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net = net.to(device)
+    dataset = MinstDataset('./data', device, batch_size=24, num_workers=2)
+    trainloader = dataset.get_trainloader()
+    testloader = dataset.get_testloader()
 
-    dataset = MinstDataset(
-        data_path='./data',
-        batch_size=24,
-        num_workers=2
-    )
-    trainloader = dataset.get_trainloder()
-    
-    num_epochs = 10
-    loss_history = train(net, num_epochs, trainloader, device, output_dir)
+    model = MinstCNN()
+    model = model.to(device)
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    last_epoch = 0
+    loss_history = {}
+
+    if checkpoint is not None:
+        last_epoch, loss_history = load_checkpoint(checkpoint, model, optimizer)
+
+    epochs = 10
+    train(model, loss_func, optimizer, epochs, trainloader,
+        checkpoint_output_dir, last_epoch, loss_history)
     plot_training_loss(loss_history)
-
-    model_path = f'{output_dir}/minst_net.pth'
-    torch.save(net.state_dict(), model_path)
-
-    testloader = dataset.get_testloder()
-    test(net, testloader, device)
+    save_model(model, f'{output_dir}/model.pth')
+    test(model, testloader)
